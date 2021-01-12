24211f847a9699e8df962f8bb1668e73eae0271a
Volpito <mpj.rigo@gmail.com>
2021-01-12 12:58:43 +0100

    modif ex03 pour un espace

--- exo_03.rb
+++ exo_03.rb
@@ -1,5 +1,5 @@
 puts "Bonjour mon petit, en quelle année es-tu né ?"
 puts "Merci de rentrer l'année complète :"
-print " >"
+print "> "
 an = gets.to_i
 puts "Ce qui fait qu'en 2017, tu avais #{2017 - an} ans ! Incroyable."
